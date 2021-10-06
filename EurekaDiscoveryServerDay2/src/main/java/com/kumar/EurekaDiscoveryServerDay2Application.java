1eb68dd9c43a38531c248f9ae4ecfb9271d6cd17
user <user@DESKTOP-R2FAUUD>
2021-10-06 19:38:34 +0530

    latest commit

--- EurekaDiscoveryServerDay2/src/main/java/com/kumar/EurekaDiscoveryServerDay2Application.java
+++ EurekaDiscoveryServerDay2/src/main/java/com/kumar/EurekaDiscoveryServerDay2Application.java
@@ -2,8 +2,10 @@ package com.kumar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaDiscoveryServerDay2Application {
 
 	public static void main(String[] args) {
