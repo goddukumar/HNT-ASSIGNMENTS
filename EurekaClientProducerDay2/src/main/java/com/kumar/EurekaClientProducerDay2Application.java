9fe8396988741e0a99d8f9df9b564a940080adbf
user <user@DESKTOP-R2FAUUD>
2021-10-06 19:51:27 +0530

    First Commit

---
+++ EurekaClientProducerDay2/src/main/java/com/kumar/EurekaClientProducerDay2Application.java
@@ -0,0 +1,13 @@
+package com.kumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientProducerDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientProducerDay2Application.class, args);
+	}
+
+}
