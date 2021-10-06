8c091dd3b850ec00f0c56a37417c5284b9d75757
user <user@DESKTOP-R2FAUUD>
2021-10-06 19:23:01 +0530

    Firt commit

---
+++ EurekaDiscoveryServerDay2/src/main/java/com/kumar/EurekaDiscoveryServerDay2Application.java
@@ -0,0 +1,13 @@
+package com.kumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServerDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServerDay2Application.class, args);
+	}
+
+}
