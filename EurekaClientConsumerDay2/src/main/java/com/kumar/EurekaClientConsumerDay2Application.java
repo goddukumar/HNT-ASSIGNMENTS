f5254b95f8067c3c661f20678ac483ac2ebbdff1
user <user@DESKTOP-R2FAUUD>
2021-10-06 19:57:16 +0530

    EurekaClientConsumer

---
+++ EurekaClientConsumerDay2/src/main/java/com/kumar/EurekaClientConsumerDay2Application.java
@@ -0,0 +1,13 @@
+package com.kumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientConsumerDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientConsumerDay2Application.class, args);
+	}
+
+}
